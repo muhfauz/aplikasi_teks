e0c27b1e3fcf592c7e042d5782e7ccabdb443ef1
Sutrisno <sutrisno.stz@bsi.ac.id>
2022-11-01 05:20:06 +0700

    mulai projek

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Belajar Form Flutter",
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController txtNama = TextEditingController();
+  String nama = "Nama";
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Coba'),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(10),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Data Input',
+                  style: TextStyle(fontSize: 25),
+                ),
+                TextField(
+                  controller: txtNama,
+                ),
+                Text(nama),
+                SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            nama = txtNama.text;
+                          });
+                        },
+                        child: Text('Klik')),
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            txtNama.text = "";
+                            nama = "";
+                          });
+                        },
+                        child: Text('Klik')),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
